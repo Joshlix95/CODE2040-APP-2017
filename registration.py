190b67dba448c646a252542639903bd0892addcf
Josuel M <Joshlix95@users.noreply.github.com>
2016-09-30 23:59:04 -0400

    Update registration.py

--- registration.py
+++ registration.py
@@ -2,4 +2,9 @@ import requests
 
 def register(token, github):
     token = "ea2915199244159af63987b64d1957c5"
+    auth = {"github": "https://github.com/Joshlix95/CODE2040-APP-2017", "token":token}
+    r = requests.post("http://challenge.code2040.org/api/register", json = auth)
     
+    return r
+
+
